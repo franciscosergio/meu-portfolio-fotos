08815cef6da87abb8b0556072b47587f3a4441d1
FranciscoSergio <franciscosergiopintodeoliveira@gmail.com>
2025-03-24 12:55:02 -0300

    Adicionando categorias de seções

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,22 +1,33 @@
 const form = document.querySelector('form');
-const nomeInput = document.getElementById('nome');
-const linkInput = document.getElementById('link');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-
-    const nome = nomeInput.value.trim();
-    const link = linkInput.value.trim();
+    
+    const nome = document.getElementById('nome').value.trim();
+    const link = document.getElementById('link').value.trim();
+    const categoria = document.getElementById('categoria').value;
+   
 
     if(!nome){
         alert('Por favor insira seu nome');
         return;
     }
 
-    if(!link || !link.startWidth('http')){
+    if(!nome || !link.startsWith('http')){
         alert('Por favor insira um link valido para a foto');
         return;
     }
+
+    const fotoDiv = document.createElement('div');
+    fotoDiv.innerHTML = `
+    <img src="${link}.jpg" alt="${nome}" style="width: 200px; height: auto;" />
+    <p>${nome}</p>
+    `;
+
+    const galeria = document.querySelector(`.fotos-${categoria}`);
+    galeria.appendChild(fotoDiv);
+
+    //document.body.appendChild(galeria);
 
     alert('Formulario enviado com sucesso!');
     form.reset();  // Limpar os campos após o envio
